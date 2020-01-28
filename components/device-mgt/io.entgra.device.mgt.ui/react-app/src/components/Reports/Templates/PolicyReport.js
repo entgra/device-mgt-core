31c39e3bc3b20c383920c227fe671fa951ed4b1d
shamalka <shamalkanavod95@gmail.com>
2020-01-28 09:38:12 +0530

    Fix date durations

--- components/device-mgt/io.entgra.device.mgt.ui/react-app/src/components/Reports/Templates/PolicyReport.js
+++ components/device-mgt/io.entgra.device.mgt.ui/react-app/src/components/Reports/Templates/PolicyReport.js
@@ -44,7 +44,9 @@ class PolicyReport extends React.Component {
         from: moment()
           .subtract(7, 'days')
           .format('YYYY-MM-DD'),
-        to: moment().format('YYYY-MM-DD'),
+        to: moment()
+          .add(1, 'days')
+          .format('YYYY-MM-DD'),
       },
       visible: false,
     };
@@ -60,10 +62,10 @@ class PolicyReport extends React.Component {
     switch (durationMode) {
       case 'daily':
         this.updateDurationValue(
-          moment()
-            .subtract(1, 'days')
-            .format('YYYY-MM-DD'),
           moment().format('YYYY-MM-DD'),
+          moment()
+            .add(1, 'days')
+            .format('YYYY-MM-DD'),
         );
         break;
       case 'weekly':
@@ -71,7 +73,9 @@ class PolicyReport extends React.Component {
           moment()
             .subtract(7, 'days')
             .format('YYYY-MM-DD'),
-          moment().format('YYYY-MM-DD'),
+          moment()
+            .add(1, 'days')
+            .format('YYYY-MM-DD'),
         );
         break;
       case 'monthly':
@@ -79,7 +83,9 @@ class PolicyReport extends React.Component {
           moment()
             .subtract(30, 'days')
             .format('YYYY-MM-DD'),
-          moment().format('YYYY-MM-DD'),
+          moment()
+            .add(1, 'days')
+            .format('YYYY-MM-DD'),
         );
         break;
     }
