86bb6cf37d04e22c8253c7af21f8938722fb5b48
Rushdi <rushdi@entgra.io>
2021-11-04 00:17:32 +0530

    Added Variables

--- components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/status/task/io/entgra/ticketing/common/TicketingHandlerConstants.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/status/task/io/entgra/ticketing/common/TicketingHandlerConstants.java
@@ -2,4 +2,11 @@ package org.wso2.carbon.device.mgt.core.status.task.io.entgra.ticketing.common;
 
 public class TicketingHandlerConstants {
     public static final String TICKETING_CONFIG_XML_NAME = "ticketing-config.xml";
+    public static final String GATEWAY_NAME = "sample";
+    public static final String ENDPOINT = "create-issue-api-endpoint";
+    public static final String AUTHORIZATION = "authorization";
+    public static final String AUTHORIZATION_KEY = "authorization-key";
+    public static final String FROM_NAME = "from-name";
+    public static final String FROM_EMAIL = "from-name";
+
 }
