c5213c8f4f31cf43762e08226ee48206fd9c3c6a
Amalka Subasinghe <amalkasubasinghe@gmail.com>
2022-08-30 20:06:11 +0530

    improved default taken handler to pass scope list

--- components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/service/api/DeviceManagementService.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/service/api/DeviceManagementService.java
@@ -2817,6 +2817,7 @@ public interface DeviceManagementService {
                     value = "Client Secret",
                     required = true)
             @PathParam("clientSecret")
-                    String clientSecret
+                    String clientSecret,
+            @QueryParam("scopes") String scopes
     );
 }
