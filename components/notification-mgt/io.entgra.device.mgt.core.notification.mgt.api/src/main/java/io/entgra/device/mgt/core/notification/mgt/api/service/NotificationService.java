a0115d1c1ce62a3117e1c68d1f070b362cc12497
NKavindya <nipunikavindya@gmail.com>
2025-09-11 18:15:10 +0530

    Add code improvements

--- components/notification-mgt/io.entgra.device.mgt.core.notification.mgt.api/src/main/java/io/entgra/device/mgt/core/notification/mgt/api/service/NotificationService.java
+++ components/notification-mgt/io.entgra.device.mgt.core.notification.mgt.api/src/main/java/io/entgra/device/mgt/core/notification/mgt/api/service/NotificationService.java
@@ -34,7 +34,6 @@ import io.swagger.annotations.Tag;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
