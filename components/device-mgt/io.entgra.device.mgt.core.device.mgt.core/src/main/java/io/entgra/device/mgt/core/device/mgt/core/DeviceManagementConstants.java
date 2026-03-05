a0ea882b4a945ef213f07bc1f4cc2122c38d7cdd
Uditha20 <udithaindunil5@gmail.com>
2026-03-05 10:28:44 +0530

    Add endpoint to retrieve BIRT report preview

--- components/device-mgt/io.entgra.device.mgt.core.device.mgt.core/src/main/java/io/entgra/device/mgt/core/device/mgt/core/DeviceManagementConstants.java
+++ components/device-mgt/io.entgra.device.mgt.core.device.mgt.core/src/main/java/io/entgra/device/mgt/core/device/mgt/core/DeviceManagementConstants.java
@@ -201,7 +201,7 @@ public final class DeviceManagementConstants {
             throw new AssertionError();
         }
         public static final String REPORTING_EVENT_HOST =  "iot.reporting.event.host";
-        public static final String REPORTING_CONTEXT = "reporting/api/analyticsadmin/v1.0/event";
+        public static final String REPORTING_CONTEXT = "/reporting/api/analyticsadmin/v1.0/event";
         public static final String DEVICE_INFO_PARAM =  "device-info";
         public static final String APP_USAGE_ENDPOINT = REPORTING_CONTEXT + "/app-usage";
         public static final String DEVICE_EVENT = "DEVICE_EVENT";
